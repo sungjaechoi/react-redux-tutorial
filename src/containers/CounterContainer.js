809184365232ad06f9415409a53234574009f087
최성재 <sungjaechoi39@gmail.com>
2024-01-12 08:47:34 +0900

    connect_2 임명함수로 작성

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -6,17 +6,13 @@ const CounterContainer = ({number, increase, decrease}) => {
   return <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
 };
 
-const mapStateToProps = state =>({
-  number:state.counter.number
-})
-
-const mapDispatchToProps = dispatch =>({
-  increase: () => {
-    dispatch(increase())
-  },
-  decrease: () => {
-    dispatch(decrease())
-  }
-})
-
-export default connect(mapStateToProps,mapDispatchToProps)(CounterContainer)
+// connect 함수 내부에서 익명함수로 선언
+export default connect(
+  state => ({
+    number:state.counter.number,
+  }),
+  dispatch =>({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease())
+  })
+)(CounterContainer)
