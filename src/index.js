8a6c05964cf92a2d3aa45e4f5331a9f85dc40af4
최성재 <sungjaechoi39@gmail.com>
2024-01-11 20:43:11 +0900

    redux_devtools 구성완료

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
