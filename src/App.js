89f2a819f3fee249815519c2be1be9d45c3f29ec
최성재 <sungjaechoi39@gmail.com>
2024-01-12 02:31:14 +0900

    connect로 counter 컴포넌트와 stor연결하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+import CounterContainer from "./containers/CounterContainer";
 import Todos from "./components/Todos";
 
 const App = () => {
   return (
     <div>
-      <Counter number={0}/>
+      {/* <Counter number={0}/> */}
+      <CounterContainer/>
       <hr/>
       <Todos/>
     </div>
