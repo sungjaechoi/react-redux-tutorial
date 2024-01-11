6943469c311d122e3767757669ffc09da3d28641
최성재 <sungjaechoi39@gmail.com>
2024-01-12 08:57:26 +0900

    connect_3 bindActionCreators 유틸함수사용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,22 +1,25 @@
-import {connect} from 'react-redux'
-import Counter from '../components/Counter'; 
-import { increase, decrease } from "../modules/counter";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({number, increase, decrease}) => {
-  return <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
 };
 
-const mapStateToProps = state =>({
-  number:state.counter.number
-})
-
-const mapDispatchToProps = dispatch =>({
-  increase: () => {
-    dispatch(increase())
-  },
-  decrease: () => {
-    dispatch(decrease())
-  }
-})
-
-export default connect(mapStateToProps,mapDispatchToProps)(CounterContainer)
+// bindActionCreators 유틸 함수를 사용 connect 함수 사용
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) =>
+    bindActionCreators(
+      {
+        increase,
+        decrease,
+      },
+      dispatch,
+    ),
+)(CounterContainer);
