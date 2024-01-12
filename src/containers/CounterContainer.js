5f3e3e286de7c0b00393a681dd7d8f62adf78a62
최성재 <sungjaechoi39@gmail.com>
2024-01-12 09:03:59 +0900

    connect_4 connect함수 내부적으로bindActionCreators 작업 대신하기

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -6,17 +6,14 @@ const CounterContainer = ({number, increase, decrease}) => {
   return <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
 };
 
-const mapStateToProps = state =>({
-  number:state.counter.number
-})
+// connect 함수 사용시 mapDispatchToProps의 인자를 객체로 넣을 시 connect함수가 내부적으로 bindActionCreators함수의 작업을 대신 해줌
 
-const mapDispatchToProps = dispatch =>({
-  increase: () => {
-    dispatch(increase())
-  },
-  decrease: () => {
-    dispatch(decrease())
+export default connect(
+  state => ({
+    number:state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
   }
-})
-
-export default connect(mapStateToProps,mapDispatchToProps)(CounterContainer)
+)(CounterContainer)
