b1884d9393cf635e977a3f3970ed7c23d67a82af
최성재 <sungjaechoi39@gmail.com>
2024-01-12 11:30:09 +0900

    counter 리덕스 모듈에 Redux-action 적용하기

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,10 +1,17 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입 정의하기
 const INCREASE = 'cunter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 //액션 생성 함수(Action Creator) 만들기
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+//redux-action 라이브러 사용 전
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+//redux-action 라이브러리 사용
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 상태값 정의하기
 const initialState = {
@@ -12,19 +19,30 @@ const initialState = {
 };
 
 // 리듀서 함수 정의하기
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+
+// redux-action 라이브러리 사용 전
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// redux-action 라이브러리 사용
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
